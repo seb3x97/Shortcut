aed529e3fb6e478c76871a67a5aecb70c1857a5b
Sébastien Voide <sebastienvoide876@gmail.com>
2023-03-23 10:08:00 +0100

    Cleanup du code test sur les threads + enlever les pycache

--- tests/test8.py
+++ tests/test8.py
@@ -21,9 +21,18 @@ class thread_with_exception(threading.Thread):
         finally:
             print('ended')
     """
+
+    def init(self):
+        print (self.ident)
+        print (self.native_id)
+        print (self.get_id())
           
     def get_id(self):
- 
+        # Check si le thread est démarré
+        if not self.is_alive: return None
+
+        #return self.ident
+
         # returns id of the respective thread
         if hasattr(self, '_thread_id'):
             return self._thread_id
@@ -31,21 +40,28 @@ class thread_with_exception(threading.Thread):
             if thread is self:
                 return id
   
-    def raise_exception(self):
-        print("exeption")
+    def raise_exception(self) -> bool:
+        # On essaye de récupérer l'identifiant du thread
         thread_id = self.get_id()
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
-              ctypes.py_object(SystemExit))
-        if res > 1:
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
-            print('Exception raise failure')
+        if thread_id is None: return False
+
+        # On essaye de fermer le thread
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(thread_id), ctypes.py_object(SystemExit))
+        if res > 1: res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(thread_id), 0)
+        if res > 1: return False
+
+        # Succès
+        return True
 
 def worker():
     for i in range(100000000000):
+        #pass
         print(str(i))
 
 thread = thread_with_exception('Thread 1', target=worker, args=())
 thread.start()
+#thread.init()
+#thread.start()
 time.sleep(2)
 thread.raise_exception()
 #thread.join()
